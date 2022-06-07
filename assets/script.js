a145a34aced801721a15ca63cdf9fd30e95810e7
Matt <mwhlavaty@gmail.com>
2022-06-07 00:14:47 -0700

    added day/week and basic time comparison function

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,18 @@
-// empty script
+// get current date and add it to header
+var currentDate = moment().format('dddd, MMMM Do');
+$('#currentDay').html(currentDate);
+
+const changeColor = function () {
+    const eventArea = $(".event-area");
+    const hourEl = $(".hour").text();
+    console.log(moment().format("hA"));
+    console.log(hourEl);
+
+    if (moment().format("hA") == hourEl) {
+        eventArea.addClass("present");
+    } else if (moment().format("hA") > hourEl) {
+        eventArea.addClass("past");
+    } else { eventArea.addClass("future") };
+}
+changeColor();
+    
