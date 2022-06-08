b369f2bb3697c6a924b9920732931e8ae80bead0
Matt <mwhlavaty@gmail.com>
2022-06-08 01:06:38 -0700

    added save and load to/from localStorage full functionality

--- README.md
+++ README.md
@@ -14,10 +14,10 @@ WHEN I view the time blocks for that day
 THEN each time block is color-coded to indicate whether it is in the past, present, or future
 
 WHEN I click into a time block
-THEN I can enter an event
+THEN I can enter an task
 
 WHEN I click the save button for that time block
-THEN the text for that event is saved in local storage
+THEN the text for that task is saved in local storage
 
 WHEN I refresh the page
-THEN the saved events persist
+THEN the saved tasks persist
--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,63 @@
 var currentDate = moment().format('dddd, MMMM Do');
 $('#currentDay').html(currentDate);
 
+var tasks = {};
+
+
+
+var loadTasks = function () {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if (!tasks) {
+        tasks = {
+            taskArr: ["", "", "", "", "", "", "", "", "", ""],
+            hourArr: ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+        };
+    }
+
+    for (let i = 0; i < tasks.hourArr.length; i++) {
+        var taskArea = tasks.taskArr[i];
+        var hour = tasks.hourArr[i];
+
+        // if taskArea has data, replace with new data in task area
+        if (taskArea != false) {
+            var replaceText = document.getElementById(`${hour}`);
+            replaceText.value = taskArea;
+        }
+    }
+};
+
+
+
+
+// add save button functionality and save task to localStorage
+$(".saveBtn").click(function () {
+    var taskArea = $(this).siblings(".task-area").val();
+    var taskTime = $(this).siblings(".hour").text();
+
+    var index = tasks.hourArr.indexOf(taskTime);
+
+    tasks.taskArr[index] = taskArea;
+
+    saveTasks();
+});
+
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+
+
 // color change functionality
 $(".hour").each(function () {
-    const eventArea = $(".event-area");
-    // console.log(moment().format("hA"));
+    const taskArea = $(".task-area");
     var timeBlock = $(this).text();
     var momentConvert = moment(timeBlock, "hA");
     if (momentConvert.diff(moment(), 'minutes') < 60 && momentConvert.diff(moment(), 'minutes') > 0) {
-        console.log("present")
-        $(this).siblings(".event-area").addClass("present");
+        $(this).siblings(".task-area").addClass("present");
     } else if (momentConvert.isAfter(moment())) {
-        console.log("future")
-        $(this).siblings(".event-area").addClass("future");
-    } else {$(this).siblings(".event-area").addClass("past")}
+        $(this).siblings(".task-area").addClass("future");
+    } else { $(this).siblings(".task-area").addClass("past") }
 });
 
+loadTasks();
